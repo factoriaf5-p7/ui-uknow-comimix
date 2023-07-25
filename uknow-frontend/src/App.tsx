225749e8b81fd6df96380dfb5d4f538936a9cc68
Raul <raul.alhena@gmail.com>
2023-07-25 19:03:41 +0200

    routes working

--- uknow-frontend/src/App.tsx
+++ uknow-frontend/src/App.tsx
@@ -1,9 +1,20 @@
 import Home from './pages/Home'
+import Hero from './pages/Hero'
+import Content from './pages/Content'
+import Dashboard from './pages/Dashboard'
+import Profile from './pages/Profile'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <Home />
+    <Routes>
+      <Route path='/' element={<Hero />} />
+      <Route path='/home' element={<Home />} />
+      <Route path='/content' element={<Content />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/profile' element={<Profile />} />
+    </Routes>
   )
 }
 
