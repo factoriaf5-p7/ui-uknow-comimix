fa5d2fb97fb86c269abde1346970a5b2af46803f
denizozerdogan <131254999+denizozerdogan@users.noreply.github.com>
2023-07-25 17:39:40 +0200

    deniz setup done

---
+++ uknow-backend/public/users/users.service.d.ts
@@ -0,0 +1,110 @@
+import { HttpStatus } from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
+import mongoose, { Model, ObjectId } from 'mongoose';
+import { User } from './schemas/user.schema';
+import { RegisterUserDto } from 'src/auth/dto/register-user.dto';
+import { RecoverUserDto } from 'src/auth/dto/recover-user.dto';
+import { RecoverRequestDto } from 'src/auth/dto/recover-request.dto';
+import { RatedCourseDto } from '../courses/dto/rate-course.dto';
+export declare class UsersService {
+    private userModel;
+    constructor(userModel: Model<User>);
+    create(user: RegisterUserDto): Promise<{
+        message: string;
+        status: HttpStatus;
+        data: string;
+    }>;
+    addCreatedCourse(userId: ObjectId, courseId: mongoose.Types.ObjectId): Promise<{
+        message: string;
+        status: HttpStatus;
+        data: string;
+    }>;
+    findAll(): Promise<{
+        message: string;
+        status: HttpStatus;
+        users: (mongoose.FlattenMaps<User> & {
+            _id: mongoose.Types.ObjectId;
+        })[];
+    }>;
+    findAllAdmin(): Promise<{
+        message: string;
+        status: HttpStatus;
+        data: (mongoose.Document<unknown, {}, User> & Omit<User & {
+            _id: mongoose.Types.ObjectId;
+        }, never>)[];
+    }>;
+    findOneLogin(email: string): Promise<mongoose.Document<unknown, {}, User> & Omit<User & {
+        _id: mongoose.Types.ObjectId;
+    }, never>>;
+    findOne(id: ObjectId): Promise<{
+        message: string;
+        status: HttpStatus;
+        data: mongoose.Document<unknown, {}, User> & Omit<User & {
+            _id: mongoose.Types.ObjectId;
+        }, never>;
+    }>;
+    findOneWithCreatedCourses(id: ObjectId): Promise<{
+        message: string;
+        status: HttpStatus;
+        data: mongoose.Document<unknown, {}, User> & Omit<User & {
+            _id: mongoose.Types.ObjectId;
+        }, never>;
+    }>;
+    findOneWithBoughtCourses(id: ObjectId): Promise<{
+        message: string;
+        status: HttpStatus;
+        data: mongoose.Document<unknown, {}, User> & Omit<User & {
+            _id: mongoose.Types.ObjectId;
+        }, never>;
+    }>;
+    update(user: UpdateUserDto): Promise<{
+        message: string;
+        status: HttpStatus;
+        data: mongoose.Document<unknown, {}, User> & Omit<User & {
+            _id: mongoose.Types.ObjectId;
+        }, never>;
+    }>;
+    updateUserByAdmin(user: UpdateUserDto): Promise<{
+        message: string;
+        status: HttpStatus;
+        data: mongoose.Document<unknown, {}, User> & Omit<User & {
+            _id: mongoose.Types.ObjectId;
+        }, never>;
+    }>;
+    updatePassword(user: RecoverUserDto): Promise<{
+        message: string;
+        status: HttpStatus;
+        data: string;
+    }>;
+    updateRecoveryToken(user: RecoverRequestDto): Promise<{
+        message: string;
+        status: HttpStatus;
+        data: mongoose.Document<unknown, {}, User> & Omit<User & {
+            _id: mongoose.Types.ObjectId;
+        }, never>;
+    }>;
+    findAllBoughtCourses(user: any, filter: any): Promise<{
+        message: string;
+        status: HttpStatus;
+        data: (mongoose.FlattenMaps<User> & {
+            _id: mongoose.Types.ObjectId;
+        })[];
+    }>;
+    deleteUserByAdmin(id: ObjectId): Promise<{
+        message: string;
+        status: HttpStatus;
+        data: string;
+    }>;
+    addRating(userId: ObjectId, ratedCourse: RatedCourseDto): Promise<{
+        message: string;
+        status: HttpStatus;
+        data: {
+            course_id: import("../courses/schemas/course.schema").Course;
+            stars: number;
+            commented: boolean;
+        }[];
+    }>;
+    updateUserBoughtCourses(userId: mongoose.Types.ObjectId, course: any): Promise<mongoose.Document<unknown, {}, User> & Omit<User & {
+        _id: mongoose.Types.ObjectId;
+    }, never>>;
+}
