09c9a2d16f8807579f4e6591d77f5cadf5b51c71
Yumi-Namie <rn.yumi@gmail.com>
2023-07-29 01:02:35 +0200

    homepage img

--- uknow-frontend/src/components/SearchBar.tsx
+++ uknow-frontend/src/components/SearchBar.tsx
@@ -1,22 +1,30 @@
-import { Stack } from '@mui/system'
+
+import { Stack } from '@mui/system';
 import InputAdornment from '@mui/material/InputAdornment';
-import { Search } from '@mui/icons-material'
+import { Search } from '@mui/icons-material';
 import { Button, OutlinedInput } from '@mui/material';
 
 function SearchBar() {
   return (
-    <Stack direction='row' >
-        <OutlinedInput id="input-with-icon-adornment" startAdornment={
-            <InputAdornment position="start">
-                <Button sx={{ paddingLeft: '0px', width: '15%'}}>
-                    <Search sx={{ paddingLeft: '0px' }}/>
-                </Button>
-            </InputAdornment>
-            }
-            sx={{ paddingLeft: '0px'}}
-        />
+    <Stack direction='row'>
+      <OutlinedInput
+        id="input-with-icon-adornment"
+        startAdornment={
+          <InputAdornment position="start">
+            <Button sx={{ paddingLeft: '0px', width: '15%'}}>
+              <Search sx={{ paddingLeft: '0px' }}/>
+            </Button>
+          </InputAdornment>
+        }
+        placeholder="Search"
+        sx={{
+          width: '100%',
+          '& .MuiOutlinedInput-input': {
+          },
+        }}
+      />
     </Stack>
-  )
+  );
 }
 
-export default SearchBar
+export default SearchBar;
--- uknow-frontend/src/pages/Home.tsx
+++ uknow-frontend/src/pages/Home.tsx
@@ -1,12 +1,17 @@
 import CourseList from "../elements/CourseList"
 import SearchBar from '../components/SearchBar';
+import uknowImg from '../assets/uknow.png'
+import { Container } from "@mui/system";
 
 function Home() {
 
 
   return (
     <div>
-        <SearchBar />
+        <img src={uknowImg} alt="Uknow image" style={{ width: '100%', height: 'auto' }} />
+        <Container  sx={{ display: 'flex', justifyContent: 'center', my: 2 }}>
+          <SearchBar />
+        </Container>
         <CourseList/>
     </div>
   )
