b547f14352586ad1578ee55eb69c5ac187b8c6ae
Yumi-Namie <rn.yumi@gmail.com>
2023-07-28 23:28:47 +0200

    card with some effects

--- uknow-frontend/src/elements/CardProduct.tsx
+++ uknow-frontend/src/elements/CardProduct.tsx
@@ -1,16 +1,16 @@
-import * as React from 'react';
+
 import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import { Grid } from '@mui/material';
+import Tooltip from '@mui/material/Tooltip';
+
 
 import { CourseData } from '../interfaces/course.interface';
 import { format } from 'date-fns';
@@ -21,130 +21,116 @@ interface CardProductProps {
   courseData: CourseData;
 }
 
-interface ExpandMoreProps extends IconButtonProps {
-  expand: boolean;
-}
+const TitleSubheaderContainer = styled('div')({
+  height: '105px', 
+  overflow: 'hidden',
+});
 
-const ExpandMore = styled((props: ExpandMoreProps) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
+const BottomGridContainer = styled(Grid)({
+  position: 'absolute',
+  bottom: 0,
+  width: '100%',
+  paddingBottom: '5px',
+  paddingTop: '5px',
+  paddingRight: '30px',
+});
+
+const CardContainer = styled(Card)({
+  position: 'relative',
+  boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+  cursor: 'pointer',
+  '&:hover': {
+    boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.2)',
+    '& $CourseDetailText': {
+      opacity: 1,
+    },
+  },
+});
 
 const CardContentContainer = styled(CardContent)(({ theme }) => ({
   maxHeight: '150px',
   overflow: 'hidden',
   marginBottom: '20px',
+  padding: '20px',
   transition: theme.transitions.create('max-height', {
     duration: theme.transitions.duration.shortest,
   }),
 }));
 
-const ExpandedCardContent = styled(CardContent)({
-  maxHeight: 'none',
-  overflow: 'auto',
-});
+const CartButton = styled(IconButton)(({ theme }) => ({
+  transition: theme.transitions.create(['transform', 'background-color', 'color'], {
+    duration: theme.transitions.duration.shortest,
+  }),
+  '&:hover': {
+    transform: 'scale(1.1)', 
+    backgroundColor: '#f8f8f8', 
+    color: theme.palette.primary.main, 
+  },
+}));
+
 
 export const CardProduct = ({ courseData }: CardProductProps) => {
-  const [expanded, setExpanded] = React.useState(false);
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-  
   const create_date = courseData.createdAt ? format(new Date(courseData.createdAt), 'MM/dd/yyyy') : '';
   const update_date = courseData.updatedAt ? format(new Date(courseData.updatedAt), 'MM/dd/yyyy') : '';
   const showDate = courseData.updatedAt ? update_date : create_date;
   
   const hasDescription = courseData.description && courseData.description.trim() !== '';
   const shortDescription = hasDescription
-  ? courseData.description.substring(0, 100) + '...'
+  ? courseData.description.substring(0, 100) + ' ...'
   : 'No description available';
 
-  const TitleSubheaderContainer = styled('div')({
-    height: '105px', 
-    overflow: 'hidden',
-  });
-
-  const BottomGridContainer = styled(Grid)({
-    position: 'absolute',
-    bottom: 0,
-    width: '100%',
-    paddingBottom: '5px',
-    paddingTop: '5px',
-  });
 
   return (
-    
-    <Card sx={{ maxWidth: 345, maxheight: 450, position: 'relative' }}>
-      
-      <CardMedia
-        component="img"
-        height="185"
-        image={courseData.image}
-        alt="image de course"
-      />
-      
-      <TitleSubheaderContainer>
-        <CardHeader
-          titleTypographyProps={{ variant: 'h6', sx: { fontSize: '1.2rem' } }}
-          title={courseData.name}
-        />
-      </TitleSubheaderContainer>
-        
+    <Tooltip title="Click to see more"  placement="right" followCursor>
+      <CardContainer sx={{ maxWidth: 345, height: 450, position: 'relative' }}>
+       
+          <CardMedia
+            component="img"
+            height="185"
+            image={courseData.image}
+            alt="image de course"
+          />
+          
+          <TitleSubheaderContainer>
+            <CardHeader
+              titleTypographyProps={{ variant: 'h6', sx: { fontSize: '1.2rem' } }}
+              title={courseData.name}
+            />
+          </TitleSubheaderContainer>
+            
 
 
-      <CardContentContainer sx={{ maxHeight: expanded ? 'none' : '150px' }}>
-        <Typography variant="body2" color="text.secondary">
-          {shortDescription}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          <RatingStars average={courseData.average} />
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Difficulty: {courseData.difficulty}
-        </Typography>
-        <Typography variant="subtitle2" sx={{ fontSize: '0.7rem' }}> {showDate}</Typography>
-      </CardContentContainer>  
-
-      <CardActions disableSpacing>
-        <BottomGridContainer container justifyContent="space-between" alignItems="center">
-          <Grid item>
-            <IconButton aria-label="Buy">
-              <AddShoppingCartIcon />
-            </IconButton>
-          </Grid>
-          <Grid item>
+          <CardContentContainer>
             <Typography variant="body2" color="text.secondary">
-              Price: ${courseData.price}
+              {shortDescription}
             </Typography>
-          </Grid>
-          <Grid item>
-            <ExpandMore
-              expand={expanded}
-              onClick={handleExpandClick}
-              aria-expanded={expanded}
-              aria-label="show more"
-            >
-              <ExpandMoreIcon />
-            </ExpandMore>
-          </Grid>
-        </BottomGridContainer>
-      </CardActions>
+            <Typography variant="body2" color="text.secondary">
+              <RatingStars average={courseData.average} />
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Difficulty: {courseData.difficulty}
+            </Typography>
+            <Typography variant="subtitle2" sx={{ fontSize: '0.7rem' }}> {showDate}</Typography>
+          </CardContentContainer>  
 
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <ExpandedCardContent>
-          <Typography paragraph>Description:</Typography>
-          <Typography paragraph>
-            {courseData.description}
-          </Typography>
-        </ExpandedCardContent>
-      </Collapse>
-    </Card>
+          <CardActions disableSpacing>
+            <BottomGridContainer container justifyContent="space-between" alignItems="center" >
+              <Grid item>
+                <CartButton aria-label="Buy">
+                  <AddShoppingCartIcon />
+                </CartButton>
+              </Grid>
+              <Grid item>
+                <Typography variant="body2" color="text.secondary">
+                  Price: ${courseData.price}
+                </Typography>
+              </Grid>
+          
+            </BottomGridContainer>
+          </CardActions>
+
+      </CardContainer>
+    </Tooltip>
   );
 };
--- uknow-frontend/src/elements/CourseList.tsx
+++ uknow-frontend/src/elements/CourseList.tsx
@@ -1,6 +1,7 @@
-import { Box, Grid, Link } from '@mui/material';
+import { Box, Grid} from '@mui/material';
 import { CardProduct } from './CardProduct';
 import { useAllCourses } from '../hooks/useQuery-AllCourses';
+import { useNavigate } from 'react-router-dom';
 
 export default function CourseList() {
   const { isLoading, isError, courseList } = useAllCourses();
@@ -9,16 +10,19 @@ export default function CourseList() {
 
   if (isError) return <div>An error has occurred while retrieving the data.</div>;
 
+  const navigate = useNavigate();
+  const handleCardClick = (courseId: string) => {
+    navigate(`/course/${courseId}`);
+  };
+
   return (
     <Box display="flex" justifyContent="center" mb={10} >
       <Grid container spacing={3} justifyContent="center" >
         {courseList?.map((course) => (
           <Grid item key={course._id} sx={{ display: { xs: 'none', sm: 'block' } }} component="div">
-            <Link to={`/course/${course._id}`}> 
-              <Box my={2} style={{ width: '330px', height: '450px' }}>
-                <CardProduct courseData={course} />
+              <Box my={2} style={{ width: '330px', height: '450px' }} onClick={() => handleCardClick(course._id)}>
+                <CardProduct courseData={course}  />
               </Box>
-            </Link>
           </Grid>
         ))}
       </Grid>
