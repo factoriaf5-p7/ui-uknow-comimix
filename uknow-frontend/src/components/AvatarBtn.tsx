5f0efcf6b9509e23a10a070a804c03d2376b6f9f
OrzeFox <dacm1997@yahoo.com>
2023-08-01 17:01:09 +0200

    fix poper 2

--- uknow-frontend/src/components/AvatarBtn.tsx
+++ uknow-frontend/src/components/AvatarBtn.tsx
@@ -1,27 +1,32 @@
-import React, { useContext, useState, useRef } from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import MenuItem from '@mui/material/MenuItem';
-import MenuList from '@mui/material/MenuList';
-import Paper from '@mui/material/Paper';
-import ClickAwayListener from '@mui/material/ClickAwayListener';
-
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Grow from '@mui/material/Grow';
+import Paper from '@mui/material/Paper';
+import Popper from '@mui/material/Popper';
+import MenuItem from '@mui/material/MenuItem';
+import MenuList from '@mui/material/MenuList';
 import { AuthContext } from '../context/AuthContex';
+ 
 
 const options = [
   { label: 'Profile', path: '/profile' },
   { label: 'Dashboard', path: '/dashboard' },
-  { label: 'Logout', path: '/login' },
+  { label: 'Logout' , path :'/login' },
 ];
 
+
+
 const AvatarBtn = () => {
-  const { logout } = useContext(AuthContext);
-  const [open, setOpen] = useState(false);
-  const anchorRef = useRef<HTMLDivElement>(null);
-  const [selectedIndex, setSelectedIndex] = useState(0);
+    const { logout } = useContext(AuthContext); 
+    const [open, setOpen] = React.useState(false);
+    const anchorRef = React.useRef<HTMLDivElement>(null);
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
+
 
   const handleClick = () => {
     setOpen((prevOpen) => !prevOpen);
@@ -35,38 +40,66 @@ const AvatarBtn = () => {
     setOpen(false);
   };
 
-  const handleClose = () => {
+  const handleClose = (event: Event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target as HTMLElement)) {
+      return;
+    }
     setOpen(false);
   };
 
   const handleLogout = () => {
-    logout();
+    logout(); 
   };
 
   return (
     <React.Fragment>
-      <ButtonGroup variant="contained" ref={anchorRef} aria-label="avatar button">
-        <Button startIcon={<AccountCircleIcon />} endIcon={<ArrowDropDownIcon />} onClick={handleClick} />
+      <ButtonGroup
+        variant="contained"
+        ref={anchorRef}
+        aria-label="avatar button"
+        onClick={handleClick}
+      >
+        <Button startIcon={<AccountCircleIcon />} endIcon={<ArrowDropDownIcon />}/>
+          
       </ButtonGroup>
-      {open && (
-        <Paper>
-          <ClickAwayListener onClickAway={handleClose}>
-            <MenuList autoFocusItem>
-              {options.map((option, index) => (
-                <MenuItem
-                  key={option.label}
-                  selected={index === selectedIndex}
-                  onClick={option.label === 'Logout' ? handleLogout : (event) => handleMenuItemClick(event, index)}
-                >
-                  <Link to={option.path} style={{ textDecoration: 'none', color: 'inherit' }}>
-                    {option.label}
-                  </Link>
-                </MenuItem>
-              ))}
-            </MenuList>
-          </ClickAwayListener>
-        </Paper>
-      )}
+      <Popper
+        sx={{
+          zIndex: 1,
+        }}
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === 'bottom' ? 'center top' : 'center bottom',
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList id="split-button-menu" autoFocusItem>
+                  {options.map((option, index) => (
+                    <MenuItem
+                      key={option.label}
+                      selected={index === selectedIndex}
+                      onClick={option.label === 'Logout' ? handleLogout : (event) => handleMenuItemClick(event, index)}
+                    >
+                      <Link to={option.path} style={{ textDecoration: 'none', color: 'inherit' }}>
+                        {option.label}
+                      </Link>
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
     </React.Fragment>
   );
 };
