516e2083b44ddc47b29027b5e1f4b0598132025a
OrzeFox <dacm1997@yahoo.com>
2023-08-01 14:22:47 +0200

    merge footer - navbar

--- uknow-frontend/src/elements/Navbar.tsx
+++ uknow-frontend/src/elements/Navbar.tsx
@@ -43,7 +43,7 @@ export default function HideAppBar() {
                     <SignupBtn />
                   </>
                 ) : (
-                  <LogoutBtn />
+                  <AvatarBtn/>
                 )}
               </div>
             </Toolbar>
