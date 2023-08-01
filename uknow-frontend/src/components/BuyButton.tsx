c4ed9757969a6f1276e16d286925ef51a3002943
denizozerdogan <131254999+denizozerdogan@users.noreply.github.com>
2023-08-01 11:40:06 +0200

    incremental changes

--- uknow-frontend/src/components/BuyButton.tsx
+++ uknow-frontend/src/components/BuyButton.tsx
@@ -7,7 +7,7 @@ const BuyButton = () => {
   return (
     <Button 
     variant="contained" color="primary" 
-    style={{ backgroundColor: UknowTheme.palette.uOrange.main, color: '#fff', marginLeft:'10px' }}
+    style={{ backgroundColor: UknowTheme.palette.uOrange.main, color: '#fff', marginLeft:'0px' }}
     sx={{ fontSize: '.8rem', padding: '0.3rem 1rem' }}>
       Buy Now 
     </Button>
--- uknow-frontend/src/components/CourseModal.tsx
+++ uknow-frontend/src/components/CourseModal.tsx
@@ -1,36 +1,54 @@
-import { Dialog, DialogContent, DialogTitle, useMediaQuery } from "@mui/material";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  useMediaQuery,
+} from "@mui/material";
 import { useTheme } from "@mui/material/styles";
-import { Typography } from '@mui/material';
+import { Typography } from "@mui/material";
 import { CourseData } from "../interfaces/course.interface";
+import BuyButton from "./BuyButton";
 
 interface CourseModalProps {
-    open: boolean;
-    onClose: () => void;
-    course: CourseData;
+  open: boolean;
+  onClose: () => void;
+  course: CourseData;
 }
-export const CourseModal = ({ open, onClose, course}: CourseModalProps) => {
+export const CourseModal = ({ open, onClose, course }: CourseModalProps) => {
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down("desktop"));
 
   return (
-    <Dialog open={open} onClose={onClose} fullScreen={fullScreen} maxWidth="desktop" fullWidth>
-  <DialogTitle>{course.name}</DialogTitle>
-  <DialogContent dividers>
-    <img src={course.image} alt={course.name} style={{ width: "100%", marginBottom: theme.spacing(2) }} />
-    <Typography>{course.description}</Typography>
-    <Typography>Price: ${course.price}</Typography>
-    <Typography>Difficulty: {course.difficulty}</Typography>
-    <Typography>Rating: {course.average}</Typography>
-{/*     <Typography>Rating: {course.showDate}</Typography>
- */}    {course.tags.map((tag) => (
-      <Typography key={tag}>{tag}</Typography>
-    ))}
-   {/*  <div style={{ maxHeight: "400px", overflowY: "auto" }}>
+    <Dialog
+      open={open}
+      onClose={onClose}
+      fullScreen={fullScreen}
+      maxWidth="desktop"
+      fullWidth
+    >
+      <DialogTitle>{course.name}</DialogTitle>
+      <DialogContent dividers>
+        <img
+          src={course.image}
+          alt={course.name}
+          style={{ width: "100%", marginBottom: theme.spacing(2) }}
+        />
+        <Typography>{course.description}</Typography>
+        <Typography>Price: ${course.price}</Typography>
+        <Typography>Difficulty: {course.difficulty}</Typography>
+        <Typography>Rating: {course.average}</Typography>
+        {/*     <Typography>Rating: {course.showDate}</Typography>
+         */}{" "}
+        {course.tags.map((tag) => (
+          <Typography key={tag}>{tag}</Typography>
+        ))}
+        {/*  <div style={{ maxHeight: "400px", overflowY: "auto" }}>
       {comments.map((comment) => (
         <Typography key={comment.id}>{comment.text}</Typography>
       ))}
     </div> */}
-  </DialogContent>
-</Dialog>
+      </DialogContent>
+      <BuyButton></BuyButton>
+    </Dialog>
   );
 };
