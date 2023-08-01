ddc9a0cdacf1e19171d89ba513507ae4df7cbca7
Yumi-Namie <rn.yumi@gmail.com>
2023-08-01 19:40:41 +0200

    card btn size an price postion

--- uknow-frontend/src/components/BuyButton.tsx
+++ uknow-frontend/src/components/BuyButton.tsx
@@ -1,17 +1,52 @@
-import { Button, } from '@mui/material'
+import { Button, useMediaQuery, } from '@mui/material'
 
 import { UknowTheme } from '../themes/ThemeUknow';
+import { useState } from 'react';
+import PurchaseModal from './PurchaseModal';
 
-const BuyButton = () => {
-  
-  return (
-    <Button 
-    variant="contained" color="primary" 
-    style={{ backgroundColor: UknowTheme.palette.uOrange.main, color: '#fff', marginLeft:'10px' }}
-    sx={{ fontSize: '.8rem', padding: '0.3rem 1rem' }}>
-      Buy Now 
-    </Button>
-  )
+interface BuyButtonProps {
+  courseId: string;
 }
 
-export default BuyButton
+const BuyButton = ({ courseId }: BuyButtonProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const isMobileScreen = useMediaQuery('(max-width: 600px)');
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handlePurchaseConfirm = () => {
+    // Implement the logic here to process the course purchase.
+    // You can use the courseId here if needed.
+    console.log("Course ID:", courseId);
+    console.log("Course purchased successfully!");
+    handleCloseModal(); // Close the modal after purchase is confirmed
+  };
+
+  return (
+    <>
+    <Button
+      variant="contained"
+      color="primary"
+      style={{
+        backgroundColor: UknowTheme.palette.uOrange.main,
+        color: '#fff',
+        marginLeft: '0px',
+        fontSize: isMobileScreen ? '1em' : '.8rem', 
+        padding: isMobileScreen ? '0.4rem 6.5rem' : '0.4rem 2rem', 
+      }}
+      onClick={handleOpenModal}
+    >
+      Buy Now
+    </Button>
+    <PurchaseModal open={isModalOpen} onClose={handleCloseModal} onConfirm={handlePurchaseConfirm} />
+  </>
+  );
+};
+
+export default BuyButton;
--- uknow-frontend/src/elements/CardProduct.tsx
+++ uknow-frontend/src/elements/CardProduct.tsx
@@ -1,16 +1,33 @@
-import { styled } from '@mui/material/styles';
+import { styled, useTheme } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import Tooltip from '@mui/material/Tooltip';
-
+import useMediaQuery from '@mui/material/useMediaQuery';
 import { CourseData } from '../interfaces/course.interface';
 import { format } from 'date-fns';
 import { CardContentSection } from '../components/CardContentSection';
 import { MouseEventHandler, useState } from 'react';
 import { CourseModal } from '../components/CourseModal';
+import BuyButton from "../components/BuyButton";
+import { CardActions, Grid, Typography } from '@mui/material';
 
+const CardActionsContainer = styled(CardActions)({
+  cursor: 'default',
+  padding: '0px 20px',
+  marginBottom: '30px',
+});
 
+const BottomGridContainer = styled(Grid)({
+  bottom: 0,
+  width: '100%',
+  paddingTop: '5px',
+  paddingLeft: '-30px',
+});
+
+const CartButtonGridItem = styled(Grid)({
+  marginLeft: "-20px",
+});
 
 interface CardProductProps {
   courseData: CourseData;
@@ -36,13 +53,7 @@ const CardContainer = styled(Card)({
 });
 
 export const CardProduct = ({ courseData }: CardProductProps) => {
-/*   const navigate = useNavigate();
-
-  const handleCardClick: MouseEventHandler<HTMLDivElement> = () => {
-    // pagina de detalle
-    navigate('/under-construction');
-  }; */
-
+  const theme = useTheme();
   const create_date = courseData.createdAt ? format(new Date(courseData.createdAt), 'MM/dd/yyyy') : '';
   const update_date = courseData.updatedAt ? format(new Date(courseData.updatedAt), 'MM/dd/yyyy') : '';
   const showDate = courseData.updatedAt ? update_date : create_date;
@@ -50,41 +61,69 @@ export const CardProduct = ({ courseData }: CardProductProps) => {
   const [modalOpen, setModalOpen] = useState(false);
 
   const handleCardClick = () => {
-    setModalOpen(true);
+    if (!modalOpen) {
+      setModalOpen(true);
+    }
   };
 
   const handleCloseModal = () => {
     setModalOpen(false);
   };
 
+  const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down('sm'));
+
   return (
     <>
-    <Tooltip title="Click to see more" placement="right" followCursor>
-      <CardContainer
-      sx={{ maxWidth: 320, height: 460, position: 'relative' }}
-      onClick={handleCardClick}>
-        <CardMedia
-          component="img"
-          height="170"
-          image={courseData.image}
-          alt="image de course"
-        />
-        <TitleSubheaderContainer>
-          <CardHeader
-            titleTypographyProps={{ variant: 'h6', sx: { fontSize: '1.2rem' } }}
-            title={courseData.name}
+    <CardContainer sx={{ maxWidth: 320, height: 470, position: 'relative' }}>
+      <Tooltip title="Click here" placement="right" followCursor>
+        <div onClick={handleCardClick}>
+          {/* Price displayed on the top side of the image */}
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            fontWeight="bold"
+            sx={{
+              color: theme.palette.uDarkBlue.main,
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              padding: '8px 12px',
+              margin: '5px',
+              background: theme.palette.background.default,
+              borderRadius: '10px',
+            }}
+          >
+            knwl: {courseData.price}
+          </Typography>
+          <CardMedia component="img" height="170" image={courseData.image} alt="image de course" />
+          <TitleSubheaderContainer>
+            <CardHeader titleTypographyProps={{ variant: 'h6', sx: { fontSize: '1.2rem' } }} title={courseData.name} />
+          </TitleSubheaderContainer>
+          <CardContentSection
+            courseData={courseData}
+            description={courseData.description}
+            average={courseData.average}
+            difficulty={courseData.difficulty}
+            showDate={showDate}
+            price={courseData.price}
           />
-        </TitleSubheaderContainer>
-        <CardContentSection
-          description={courseData.description}
-          average={courseData.average}
-          difficulty={courseData.difficulty}
-          showDate={showDate}
-          price={courseData.price}
-        />
-      </CardContainer>
-    </Tooltip>
+        </div>
+      </Tooltip>
+      <CardActionsContainer disableSpacing sx={{ padding: '0px 20px', marginBottom: '30px' }}>
+        <BottomGridContainer container justifyContent="space-between" alignItems="center">
+          <Grid item>
+            {/* Remove the price display from here */}
+          </Grid>
+          <CartButtonGridItem item>
+            <BuyButton
+              courseId={courseData._id}
+              sx={isSmallScreen ? { fontSize: '1em', padding: '0.5rem 1.2rem' } : undefined}
+            />
+          </CartButtonGridItem>
+        </BottomGridContainer>
+      </CardActionsContainer>
+    </CardContainer>
     <CourseModal open={modalOpen} onClose={handleCloseModal} course={courseData} />
-    </>
+  </>
   );
 };
