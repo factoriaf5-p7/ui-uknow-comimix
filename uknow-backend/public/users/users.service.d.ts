eac9ececd6e859176d24c3a3f839f3a243c100db
OrzeFox <dacm1997@yahoo.com>
2023-08-02 12:10:58 +0200

    merge public

--- uknow-backend/public/users/users.service.d.ts
+++ uknow-backend/public/users/users.service.d.ts
@@ -105,7 +105,7 @@ export declare class UsersService {
             course_id: import("../courses/schemas/course.schema").Course;
             stars: number;
             commented: boolean;
-        }[], 0 | 1 | -1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20>[];
+        }[], 0 | 1 | -1 | 2 | 3 | 5 | 4 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20>[];
     }>;
     findBoughtCourses(user: any, filter: any): Promise<{
         message: string;
