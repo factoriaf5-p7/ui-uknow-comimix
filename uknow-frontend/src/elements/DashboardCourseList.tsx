9e727088366b17ff348b1ba0c41ba87ec2864fe4
Raul <raul.alhena@gmail.com>
2023-08-02 11:43:18 +0200

    list of created courses retrieved and showed

--- uknow-frontend/src/elements/DashboardCourseList.tsx
+++ uknow-frontend/src/elements/DashboardCourseList.tsx
@@ -7,43 +7,39 @@ import { ListItem } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { ListItemText } from '@mui/material';
-
-
-function generate(element: React.ReactElement) {
-    return [0, 1, 2].map((value) =>
-      React.cloneElement(element, {
-        key: value,
-      }),
-    );
-  }
+import { useCreatedCourses } from '../services/useCreatedCourses';
+import { EditNote } from '@mui/icons-material';
 
 function DashboardCourseList() {
 
-    const [dense, setDense] = React.useState(false);
-    const [secondary, setSecondary] = React.useState(false);
+    const { isLoading, isError, courseList } = useCreatedCourses('64c7a7ccbb9cc787c92592f9');
 
     return (
-        <Box>
+        <Box sx={{ border: '1px solid blue', marginTop: '80px', paddingLeft: '10px' }}>
         <Grid item xs={12} md={6}>
             <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-                Avatar with text and icon
-            </Typography>
-            
-                <List dense={dense}>
-                {generate(
-                    <ListItem
-                    secondaryAction={
-                        <IconButton edge="end" aria-label="delete">
-                        <DeleteIcon />
-                        </IconButton>
-                    }
-                    >
-                    <ListItemText
-                        primary="Single-line item"
-                        secondary={secondary ? 'Secondary text' : null}
-                    />
-                    </ListItem>,
-                )}
+                    Created courses
+            </Typography>  
+                <List>
+                {isLoading ? 
+                    ( <div>Loading courses...</div> )
+                    :
+                isError ?
+                    ( <div>Error consultando los cursos</div>)
+                    :
+                    (
+                        courseList && courseList.map(course => {
+                            return (<ListItem key={course._id} secondaryAction={
+                                            <div>
+                                                <IconButton edge="end" aria-label="edit"> <EditNote /> </IconButton>
+                                                <IconButton edge="end" aria-label="delete"> <DeleteIcon /> </IconButton>
+                                            </div>
+                                        }>
+                                <ListItemText primary={course.name}/>
+                                </ListItem>)
+                        })
+                    )
+                }
                 </List>
             </Grid>
         </Box>
