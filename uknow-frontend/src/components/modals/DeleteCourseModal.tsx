e3ca64f169dacb6b57b7f9f2a56aba5cb1ee194f
Raul <raul.alhena@gmail.com>
2023-08-04 04:08:15 +0200

    created delete course modal

---
+++ uknow-frontend/src/components/modals/DeleteCourseModal.tsx
@@ -0,0 +1,35 @@
+import { Dialog, DialogActions, DialogContent, DialogTitle, Button, useTheme } from '@mui/material';
+
+interface DeleteCourseModalProps {
+  open: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+}
+
+function DeleteCourseModal({ open, onClose, onConfirm }: DeleteCourseModalProps) {
+  const theme = useTheme();
+
+  return (
+    <Dialog open={open} onClose={onClose} >
+      <DialogTitle>Confirm DELETE course?</DialogTitle>
+      <DialogContent >
+        <p>Are you sure you want to DELETE the course?</p>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} color="primary" sx={{ backgroundColor: theme.palette.uDarkBlue.main, color: 'white' }}>
+          No
+        </Button>
+        <Button
+          onClick={onConfirm}
+          color="primary"
+          autoFocus
+          sx={{ backgroundColor: theme.palette.uDarkBlue.main, color: 'white' }}
+        >
+          Yes
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+export default DeleteCourseModal;
